a2f888bf208e71d497033f1f72b1399894f81b79
Andrii <55akif5548@gmail.com>
2022-04-10 11:43:19 +0300

    Применен усилитель store (enhancer)

--- src/store/index.js
+++ src/store/index.js
@@ -2,7 +2,22 @@ import { createStore, combineReducers } from 'redux';
 import heroes from '../reducers/heroes';
 import filters from '../reducers/filters';
 
-const store = createStore(combineReducers({ heroes, filters }),
-    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__());
+const enhancer = (creatStore) => (...args) => {
+    const store = creatStore(...args);
+
+    const oldDispatch = store.dispatch;
+    store.dispatch = (action) => {
+        if (typeof action === 'string') {
+            return oldDispatch({
+                type:action
+            })
+        }
+        return oldDispatch(action)
+    }
+    return store;
+}
+
+const store = createStore(combineReducers({ heroes, filters }), enhancer);
 
 export default store;
+// window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
